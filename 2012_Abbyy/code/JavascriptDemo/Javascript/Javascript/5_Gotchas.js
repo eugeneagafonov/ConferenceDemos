62c69a5cff17b48d59ceacf690b712bdf460668b
Eugene Agafonov <eugene.agafonov@gmail.com>
2012-08-15 18:20:31 +0400

    continue adding js demos

--- 2012_Abbyy/code/JavascriptDemo/Javascript/Javascript/5_Gotchas.js
+++ 2012_Abbyy/code/JavascriptDemo/Javascript/Javascript/5_Gotchas.js
@@ -4,6 +4,128 @@
 ///<reference path="Scripts/date.js" />
 
 // -------------------------------------------
-// Gotchas\
+// Gotchas
+// 1. Chain assignement
+//var a = 2;
+
+//var a, b;
+//a = b = 2;
+
+//var a = b = 2; // b - global
+//if(window.b) {
+//    writeLine("b is a global variable");
+//}
+
+
+//(function () {
+//    var fun = function() {
+//        writeLine(a); // defined to undefined
+//        //writeLine(b);
+//        var a = 20;
+//        writeLine(a);
+//    };
+
+//    fun();
+
+//}());
+
+//(function () {
+//    var i;
+
+//    i = 199;
+    
+//    for (var i = 0; i < 10; i++)
+//        ;
+
+//    writeLine(i);
+
+//}());
+
+//var i = 20;
+//(function () {
+    
+//    writeLine(i);
+    
+//    var i = 50;
+
+//    writeLine(i);
+//}());
+
+//writeLine(i);
+
+//function foo() {
+//    function bar() {
+//        return 3;
+//    }
+//    return bar();
+//    function bar() {
+//        return 8;
+//    }
+//}
+//writeLine(foo());
+
+//function foo() {
+//    var bar = function () {
+//        return 3;
+//    };
+//    return bar();
+//    var bar = function () {
+//        return 8;
+//    };
+//}
+//writeLine(foo());
+
+//writeLine(foo());
+//function foo() {
+//    var bar = function () {
+//        return 3;
+//    };
+//    return bar();
+//    var bar = function () {
+//        return 8;
+//    };
+//}
+
+//function foo() {
+//    return bar();
+//    var bar = function () {
+//        return 3;
+//    };
+//    var bar = function () {
+//        return 8;
+//    };
+//}
+//writeLine(foo());
+
+//// declare all vars at the very first line of a function
+
+// -------------------------------------------------------------
+
+//writeLine(parseInt("10"));
+//writeLine(parseInt("010")); // IE10 - wtf??
+//writeLine(parseInt("0x10"));
+//writeLine(parseInt("09"));
+//writeLine(parseInt("09", 10));
+
+//var a = "09" - 1;
+//writeLine(a);
+
+//var a = "09" + "010";
+//writeLine(a);
+
+//var a = + "09" + "10";
+//writeLine(a);
+
+//var a = + "09" + "010";
+//writeLine(a);
+
+//var a = + "09" + ( + "010");
+//writeLine(a);
+
+//var a = "3" * "010";
+//writeLine(a);
+// ---------------------------------------------------------------
+
+
 
 
